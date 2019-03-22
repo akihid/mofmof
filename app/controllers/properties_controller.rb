9920d7d178adbbd6979da7dc4ce21ff51ee83dee
sato akihide <aki@sato.local>
2019-03-23 01:47:10 +0900

    更新処理実装

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,32 +1,52 @@
 class PropertiesController < ApplicationController
+  before_action :set_property ,only:[:edit, :update , :show ,:destroy]
+
   def index
     @properties = Property.all
   end
   def new
     @property = Property.new()
 
-    2.times { @property.closet_stations.build }
+    2.times { @property.closet_stations.new() }
   end
 
   def create
-    @property = Property.new(set_params)
-    # @property.closet_stations.property_id = @property.id
-    if @property.save!
+    @property = Property.new(property_params)
+    if @property.save
       redirect_to properties_path
     else
       render 'new'
     end
   end
 
+  def edit
+  end
+
+  def update
+    
+    if @property.update(property_params)
+      redirect_to properties_path
+    else
+      render 'edit'
+    end
+  end
+
   def show
-    @property = Property.find(params[:id])
     @closet_stations = ClosetStation.where(property_id: params[:id])
   end
 
-  private
+  def destroy
+    @property.destroy
+    redirect_to properties_path
+  end
 
-  def set_params
+  private
+  def property_params
     params.require(:property).permit(:name, :rent ,:address, :age, :remark ,
-       closet_stations_attributes: [:close_number, :route_name, :station_name, :walikng_to_closest_station_minutes])
+       closet_stations_attributes: [:close_number, :route_name, :station_name, :walikng_to_closest_station_minutes, :id])
+  end
+
+  def set_property
+    @property = Property.includes(:closet_stations).find(params[:id])
   end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,4 @@
 class Property < ApplicationRecord
-  has_many :closet_stations
+  has_many :closet_stations, dependent: :destroy
   accepts_nested_attributes_for :closet_stations
 end
