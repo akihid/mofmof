911e6449edd93ef0320513be49d59ee202f7ad6a
sato akihide <aki@sato.local>
2019-03-23 01:48:29 +0900

    不要ファイル削除

--- app/controllers/closet_stations_controller.rb
+++
@@ -1,4 +0,0 @@
-class ClosetStationsController < ApplicationController
-  def new
-  end
-end
--- test/controllers/closet_stations_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ClosetStationsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get closet_stations_new_url
-    assert_response :success
-  end
-
-end
